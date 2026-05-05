e86de54992fbbc5666b224df7aa96bef2db32335
arobles97-byte <arobles97@toromail.csudh.edu>
2026-05-05 09:33:53 -0700

    Update sort.c

--- Gradebook/sort.c
+++ Gradebook/sort.c
@@ -1,1 +1,23 @@
 //sort function//
+#include "sort.h"
+
+void sortById(Student students[], int count) {
+
+  int i;
+  int j;
+
+for (i = 0; i < count - 1; i++) {
+
+    for (j = 0; j < count - 1; j++) {
+
+      if (students[j].id > students[j + 1].id) {
+
+          Student temp;
+
+          temp = students[j];
+          students[j] = students[j + 1];
+          students[j + 1] = temp;
+      }
+    }
+  }
+}
