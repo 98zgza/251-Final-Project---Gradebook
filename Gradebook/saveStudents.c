8a907d3c67bf015f8df9655c83895645a8bbdcea
Bubba046 <140658589+Bubba046@users.noreply.github.com>
2026-05-06 13:14:44 -0700

    Create saveStudents.c

---
+++ Gradebook/saveStudents.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "saveStudents.h"
+
+void saveStudents(Student students[], int student_count)
+{
+
+FILE *myFile = fopen("Gradebook.txt", "w");
+
+if (myFile == NULL)
+{
+        printf("No file to write to\n");
+        return;
+}
+
+for (int i = 0; i < student_count; i++)
+{
+        fprintf(myFile, "\n");
+        fprintf(myFile, "Name:%s\t id:%d\n", students[i].student_name,students[i].id);
+
+        for (int j = 0; j < students[i].grade_count;j++)
+        {
+                fprintf(myFile, "%s\t%d\n", students[i].grades[j].assignment_name,
+                                                        students[i].grades[j].grade);
+        }
+
+        fprintf(myFile, "\n");
+}
+fclose(myFile);
+printf("Student info saved to gradebook");
+}
+
+
+
+
+
+
+
