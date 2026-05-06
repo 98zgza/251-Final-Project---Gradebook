76e1c36c4dbe99b7e5622065c1d3cb97874b0a6b
98zgza <98zurielg@gmail.com>
2026-05-06 11:54:47 +0000

    student header updated

--- Gradebook/student.h
+++ Gradebook/student.h
@@ -1,1 +1,23 @@
 //student header//
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#define NAME_LIMIT 30
+#define GRADE_LIMIT 10
+#define STUDENT_LIMIT 30
+
+typedef struct
+{
+    char assignment_name[NAME_LIMIT];
+    int grade;
+}Grade;
+
+typedef struct
+{
+    char student_name[NAME_LIMIT];
+    Grade grades[GRADE_LIMIT];
+    int grade_count;
+
+}Student;
+
+#endif
