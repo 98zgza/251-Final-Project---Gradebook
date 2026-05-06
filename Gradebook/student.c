c03369460c9239651d34c9bd8bae0a465eb65bfc
98zgza <98zurielg@gmail.com>
2026-05-06 13:20:16 +0000

    student.c added

--- Gradebook/student.c
+++ Gradebook/student.c
@@ -1,1 +1,10 @@
 //student function//
+#include <stdio.h>
+#include <string.h>
+#include "student.h"
+
+void initalizeStudent(Student* s,const char name[])
+{
+    strncpy(s->student_name,name,NAME_LIMIT -1);
+    s->student_name[NAME_LIMIT-1] = '\0';
+}
