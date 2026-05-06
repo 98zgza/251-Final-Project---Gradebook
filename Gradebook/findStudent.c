5e6acafbffad7c7a1ab2d04c72d1608e1c14af4a
98zgza <98zurielg@gmail.com>
2026-05-06 13:28:12 +0000

    findstudent.c added

---
+++ Gradebook/findStudent.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "student.h"
+#include "findStudent.h"
+
+int findStudent(Student students[], int student_count)
+{
+    char searchName[NAME_LIMIT];
+
+    printf("Enter student name to find: ");
+    fgets(searchName, NAME_LIMIT, stdin);
+
+    for (int i = 0; i < student_count; i++)
+    {
+        if (strcmp(students[i].student_name, searchName) == 0)
+        {
+            printf("Student found: %s\n", students[i].student_name);
+            return i;
+        }
+    }
+
+    printf("Student not found.\n");
+    return -1;
+}
