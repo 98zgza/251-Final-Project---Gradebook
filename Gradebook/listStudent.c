a73d5218f1c0599ae99b1e1d8cca9fd49ce3c332
98zgza <98zurielg@gmail.com>
2026-05-06 16:24:26 +0000

    fixed errors

--- Gradebook/listStudent.c
+++ Gradebook/listStudent.c
@@ -5,16 +5,16 @@
 void listStudents(Student students[], int student_count)
 {
     if (student_count == 0)
-{
-      printf("No students available.\n");
-      return;
-}
+    {
+        printf("No students available.\n");
+        return;
+    }
 
-for (int i = 0; i < student_count; i++)
-{
-      printf("Student %d:\n", i + 1);
-      printf("Nmae: %s\n", students[i].student_name);
-      printf("Grades: %d\n", students{i}.grade_count);
-      printf("\n");
-  }
+    for (int i = 0; i < student_count; i++)
+    {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s\n", students[i].student_name);
+        printf("Grades: %d\n", students[i].grade_count);
+        printf("\n");
+    }
 }
